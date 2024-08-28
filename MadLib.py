5313157671e3622725770323c226385f0a6d3a1f
LoganB23 <lbaardson@unomaha.edu>
2024-08-28 16:47:14 +0000

    almost finished mad lib

--- MadLib.py
+++ MadLib.py
@@ -10,10 +10,12 @@ def main():
   animal = input("enter an animal ")
   noun = input("enter a noun ")
   animal2 = input("enter an animal ")
+  adjective = input("enter an adjective ")
+  verb = input("enter a verb ")
   #Print the story with the user supplied words.
   print("one fine Sunday afternoon, your good friend", friend, "was walking his pet", animal)
-  print("around a", noun, ". Suddenly, out of nowhere, a big, fat, angry,", animal2, "charged at", friend, "!")
-  print(friend, "was absolutely terrified!")
+  print("around a", noun, ". Suddenly, out of nowhere, a big, fat, angry,", animal2, "charged at", friend,"!")
+  print(friend, "was absolutely terrified! Thankfully, the", animal, "was very", adjective, "which made the", animal2, verb)
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
