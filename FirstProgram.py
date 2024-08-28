6c53642d888c391f9fd5a71e39243507f9db4b9f
LoganB23 <lbaardson@unomaha.edu>
2024-08-28 16:40:52 +0000

    trial run

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,22 +1,23 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Logan Baardson
+#Date: 8/28
+#Assignment: First program
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("Hello, how are you doing fine sir?")
   #Ask for the user's name
-
+  name = input("What is your name? ")
   #Use the user's name in the program.
-
+  print("What a wonderful name you have,", name)
   #Ask the user for their age.
-
+  age = input("How old are you? ")
   #Tell the user what year they were born in.
+  age = int(age)
+  birthYear = 2024 - age
   #Assume that they have not had their birthday yet this year.
-
-
+  print("Wow! That means that you were born in", birthYear)
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
--- MadLib.py
+++ MadLib.py
@@ -1,16 +1,19 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Logan Baardson
+#Date: 8/27
+#Assignment: Mad lib
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  friend = input("enter the name of one of your friends ")
+  animal = input("enter an animal ")
+  noun = input("enter a noun ")
+  animal2 = input("enter an animal ")
   #Print the story with the user supplied words.
-
-
-
+  print("one fine Sunday afternoon, your good friend", friend, "was walking his pet", animal)
+  print("around a", noun, ". Suddenly, out of nowhere, a big, fat, angry,", animal2, "charged at", friend, "!")
+  print(friend, "was absolutely terrified!")
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
