1019dd071effa9b9a9a2df59d39a3b6a461a7e09
LoganB23 <lbaardson@unomaha.edu>
2024-08-29 22:26:12 +0000

    mad lib finished product

--- MadLib.py
+++ MadLib.py
@@ -8,14 +8,14 @@ def main():
   #Ask user for words
   friend = input("enter the name of one of your friends ")
   animal = input("enter an animal ")
-  noun = input("enter a noun ")
+  location = input("enter the name of a city ")
   animal2 = input("enter an animal ")
-  adjective = input("enter an adjective ")
+  weapon = input("enter a type of weapon ")
   verb = input("enter a verb ")
   #Print the story with the user supplied words.
-  print("one fine Sunday afternoon, your good friend", friend, "was walking his pet", animal)
-  print("around a", noun, ". Suddenly, out of nowhere, a big, fat, angry,", animal2, "charged at", friend,"!")
-  print(friend, "was absolutely terrified! Thankfully, the", animal, "was very", adjective, "which made the", animal2, verb)
+  print("One fine Sunday afternoon, your good friend", friend, "was walking his pet", animal)
+  print("around", location, ". Suddenly, out of nowhere, a big, fat, angry,", animal2, "charged at", friend,"!")
+  print(friend, "was absolutely terrified! Thankfully, the pet", animal, "was armed with a", weapon, "which made the", animal2, verb)
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
